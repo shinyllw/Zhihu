bea8985f7a887787ec53816b5eab897b3db47d40
wll <shinyllw@gmail.com>
2018-12-05 20:54:26 +0800

    add distributed

--- zhihuuser/settings.py
+++ zhihuuser/settings.py
@@ -75,7 +75,7 @@ DEFAULT_REQUEST_HEADERS = {
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     'zhihuuser.pipelines.MongoPipeline': 300,
-    # 'scrapy_redis.pipelines.RedisPipeline': 301
+    'scrapy_redis.pipelines.RedisPipeline': 301
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
@@ -107,8 +107,7 @@ ITEM_PIPELINES = {
 MONGO_URI = 'localhost'
 MONGO_DATABASE = 'zhihu'
 
-# SCHEDULER = "scrapy_redis.scheduler.Scheduler"
-
-# DUPEFILTER_CLASS = "scrapy_redis.dupefilter.RFPDupeFilter"
-
+SCHEDULER = "scrapy_redis.scheduler.Scheduler"
+DUPEFILTER_CLASS = "scrapy_redis.dupefilter.RFPDupeFilter"
+REDIS_URL = '127.0.0.1:6379'
 # SCHEDULER_FLUSH_ON_START = True
